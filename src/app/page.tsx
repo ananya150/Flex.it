709fddf1f08880e21d6f48b060a96d883e96cb84
ananya150 <akblockchain15@gmail.com>
2024-02-02 23:05:15 +0530

    updated navbar style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useRef } from "react";
 
 export default function Home() {
   return (
-    <div className="h-[200vh]" >
+    <div className="h-[300vh] bg-[#0D0D0D]" >
       <Navbar />
     </div>
   )
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import { Link } from 'lucide-react';
 
 const Navbar = () => {
 
@@ -10,8 +11,14 @@ const Navbar = () => {
     
         const handleScroll = () => {
           const currentScrollY = window.pageYOffset;
-    
-          setIsVisible(lastScrollY > currentScrollY );
+
+        //   if (currentScrollY > 100) {
+            setIsVisible(lastScrollY > currentScrollY);
+        //   } else {
+        //     // Always visible within the first 100px
+        //     setIsVisible(true);
+        //   }    
+
           lastScrollY = currentScrollY;
         };
     
@@ -22,15 +29,21 @@ const Navbar = () => {
 
 
   return (
-    <nav style={{
-        position: 'fixed',
-        width: '100%',
-        backgroundColor: 'black',
-        transition: 'top 0.3s',
-        top: isVisible ? '0' : '-100px', // Adjust -100px as needed
-      }}>        
-      <span className='text-white'>Nav</span>
-      </nav>  
+        <div className='flex w-[100%] justify-center'>
+            <div style={{transition: 'top 0.4s'}} className={`fixed w-[92%] bg-[#0D0D0D] ${isVisible ? 'top-5': 'top-[-100px]'} h-[85px] rounded-[3rem] border-[#1D1D1D] border flex flex-col justify-center px-12 `}>
+                <div className='w-full flex justify-between items-center'>
+                    <div className='flex items-center space-x-2'>
+                        <Link className='text-white w-7 h-7' />
+                        <span className='font-bold text-white text-[32px] font-sat'>flex.it</span>
+                    </div>
+                    <div className='flex items-center space-x-16'>
+                        <span className='text-white text-[18px] font-sat font-medium'>Home</span>
+                        <span className='text-white text-[18px] font-sat font-medium'>Blog</span>
+                        <span className='text-white text-[18px] font-sat font-medium'>FAQ</span>
+                    </div>
+                </div>        
+            </div>  
+        </div>
     )
 }
 
