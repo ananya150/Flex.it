ae1b6f44cf8a4c8ea2187bf4cd04236e460baf95
ananya150 <akblockchain15@gmail.com>
2024-02-05 19:26:09 +0530

    added amount inputs and connect wallet from wagmo

--- src/components/create/Create.tsx
+++ src/components/create/Create.tsx
@@ -1,48 +1,109 @@
 "use client";
-import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 import { Tabs } from './Tabs';
 import { WavyBackground } from './WavyBackground';
+import { Link } from 'lucide-react';
+import { useAccount } from 'wagmi';
+import { useModal } from 'connectkit';
 
 const CreateLink = () => {
 
     const [activeTab, setActiveTab] = useState(0);
-    const [background, setBackground] = useState('bg-purple-500'); // Default background
+    const [image, setImage] = useState('');
+    const [message, setMessage] = useState('');
+    const [amount, setAmount] = useState('');
+    const {isConnected, address} = useAccount()
+    const [connected , setConnected] = useState(false);
+    const [balance, setBalance] = useState('0.0')
+
+    const imputRef1 = useRef(null);
+    const inputRef2 = useRef(null)
 
     useEffect(() => {
-        const handleMouseMove = (event: any) => {
-          const windowWidth = window.innerWidth;
-          const windowHeight = window.innerHeight;
-          
-          const mouseXpercentage = Math.round((event.pageX / windowWidth) * 100);
-          const mouseYpercentage = Math.round((event.pageY / windowHeight) * 100);
-          
-          const newBackground = `radial-gradient(at ${mouseXpercentage}% ${mouseYpercentage}%, #3498db, #9b59b6)`;
-          setBackground(newBackground);
-        };
-    
-        document.addEventListener('mousemove', handleMouseMove);
-    
-        return () => {
-          document.removeEventListener('mousemove', handleMouseMove);
-        };
-      }, []);
-    
+        setConnected(isConnected);
+        // clearStorage()
+        if(isConnected){
+            // fetchBalance()
+        }
+    }, [isConnected, address])
+
+    const {setOpen} = useModal({onDisconnect: () => {
+        setBalance('0.0');
+    }});
+
+    const handleConnectWallet = () => {
+        if(!isConnected){
+            setOpen(true);
+            return;
+        }
+    }
+
+    const handleAmountChange = (event: any) => {
+        const { value } = event.target;
+
+        if (/^[0-9]*\.?[0-9]{0,2}$/.test(value) || value === "") {
+            setAmount(value);
+        } else if (event.target.value === '') {
+        setAmount('');
+        }
+    }
+
+    useEffect(() => {
+
+    },[amount])
 
     return (
-        <div>
-            <div className='px-24 mt-[160px] flex items-end justify-center w-full'>
-                <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
-            </div>
-            <div className='mt-20 w-full flex justify-center'>
-                <div className='w-[330px] h-[450px] rounded-3xl overflow-hidden relative flex flex-col justify-start '>
-                    <div className='w-full flex justify-center z-20 absolute top-[180px]'>
-                        <span className='text-white text-[45px] font-sat font-bold'>$ 15.00</span>
-                    </div>
-                    {/* <div className='mx-[10px] w-[310px] bg-white bottom-[10px] z-10 h-[50px] absolute rounded-xl'>
-
-                    </div> */}
-                    <WavyBackground />
+        <div className='flex w-full justify-evenly'>
+            <div className='flex flex-col items-center px-24'>
+                <div className='mt-[180px] flex items-end ml-3'>
+                    <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
                 </div>
+                {
+                    activeTab === 0 && 
+                    <div className='mt-20'>
+                        <div className='w-[330px] h-[450px] rounded-3xl overflow-hidden relative flex flex-col justify-start '>
+                            <div className='w-full flex justify-center z-20 absolute top-[180px]'>
+                                <span className='text-white text-[45px] font-sat font-bold'>$</span>
+                                <div className='max-w-[270px] overflow-clip'>
+                                    <span className='text-white text-[45px] font-sat font-bold'>{amount}</span>
+                                </div>
+                                1<input value={amount} onChange={handleAmountChange} autoFocus className='outline-none border-none bg-transparent w-[1px] text-[45px] text-white' />
+                                <span className={`text-gray-400 text-[45px] font-sat font-bold ${amount.length === 0 ? '': 'hidden'}`}>0.0</span>
+                            </div>
+                            <div className='w-full flex justify-start items-center z-20 absolute space-x-2 top-[15px] px-[15px]'>
+                                <Link className='text-gray-400 w-4 h-4' />
+                                <span className='font-semibold text-gray-400 text-[18px] font-sat'>flex.it</span>                    
+                            </div>
+                            <WavyBackground />
+                        </div>
+                    </div>
+                }
+                {
+                    activeTab === 1 &&
+                    <div className='mt-20'>
+                        <div className='w-[330px] bg-white h-[450px] rounded-3xl overflow-hidden relative flex flex-col justify-start '>
+                            <div className='w-full flex justify-center z-20 absolute top-[180px]'>
+                                <span className=' text-[45px] font-sat font-bold'>$ 15.00</span>
+                            </div>
+                            <div className='w-full flex justify-start items-center z-20 absolute space-x-2 top-[15px] px-[15px]'>
+                                <Link className='text-gray-800 w-4 h-4' />
+                                <span className='font-semibold text-gray-800 text-[18px] font-sat'>flex.it</span>                    
+                            </div>
+                        </div>
+                    </div>
+                }
+            </div>
+            <div className='flex flex-col justify-center'>
+                {
+                    connected ?
+                    <div className='w-[130px] mt-40 bg-white h-[45px] rounded-lg hover:bg-[#0D0D0D] hover:text-white duration-200 cursor-pointer flex justify-center items-center'>
+                        <span className='font-medium transition-[1s] text-[18px]'>Create Link</span>
+                    </div>
+                        :
+                    <div onClick={handleConnectWallet} className='w-[130px] mt-40 bg-white h-[45px] rounded-lg hover:bg-[#0D0D0D] hover:text-white duration-200 cursor-pointer flex justify-center items-center'>
+                        <span className='font-medium transition-[1s] text-[15px]'>Connect Wallet</span>
+                    </div>
+                }
             </div>
         </div>
     )
